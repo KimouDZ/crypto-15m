5877fbc262a9be27fed686f2ef688266ec4e494f
KimouDZ <81564317+KimouDZ@users.noreply.github.com>
2025-07-23 09:02:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ function sendTelegramMessage(message) {
 }
 
 function formatDate(date) {
-  const offsetDate = new Date(date.getTime() + 1 * 60 * 60 * 1000); // GMT+1
-  return offsetDate.toLocaleString('en-GB', {
+  return date.toLocaleString('en-GB', {
     day: '2-digit', month: '2-digit', year: 'numeric',
     hour: '2-digit', minute: '2-digit', hour12: false
   }).replace(',', ' -');
